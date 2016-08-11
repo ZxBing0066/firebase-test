e0b84c5a70eff8831a7459eef331c2c158a02653
ZxBing0066 <ZxBing0066@gmail.com>
2016-08-11 18:13:11 +0800

    add ui context

--- src/js/AppWrapper.jsx
+++ src/js/AppWrapper.jsx
@@ -4,13 +4,36 @@ import Header from './components/Header.jsx';
 import AppDrawer from './components/AppDrawer.jsx';
 
 export default React.createClass({
+    childContextTypes: {
+        ui: React.PropTypes.object
+    },
+    getChildContext: function() {
+        return {
+            ui: {
+                // showLoading: () => this.showLoading(),
+                // hideLoading: () => this.hideLoading(),
+                alert: (...args) => window.alert(...args),
+                confirm: (...args) => window.confirm(...args),
+                showHeader: () => this.showHeader(),
+                hideHeader: () => this.hideHeader(),
+            }
+        };
+    },
+    showHeader() {
+        this.refs.header.show();
+    },
+    hideHeader() {
+        this.refs.header.hide();
+    },
     render() {
         return (
             <div>
-                <Header />
+                <Header ref='header' />
                 <AppDrawer />
-                {this.props.children}
+                <div style={{padding: '0 5px'}}>
+                    {this.props.children}
+                </div>
             </div>
         );
     }
-})
+});
